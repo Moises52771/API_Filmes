273499658639f9ddf023458eb877dd3f7612ec9d
Moises <moises.dev52771@gmail.com>
2022-02-21 23:39:22 -0300

    Inicio

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,25 @@
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+type CategoryRequest = {
+    name: string;
+    description: string;
+}
+
+export class CreateCategoryService {
+    async execute({name, description}:CategoryRequest): Promise<Category>{
+        const repo = getRepository(Category);
+
+        //
+
+        const category = repo.create({
+            name,
+            description
+        });
+
+        await repo.save(category);
+
+        return category;
+
+    }
+}
