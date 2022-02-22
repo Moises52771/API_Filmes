2a52e26bfffea024cad9a70d30ef1374ea59a392
Moises <moises.dev52771@gmail.com>
2022-02-22 16:00:33 -0300

    CRUD de categories finalizado

--- src/entities/Category.ts
+++ src/entities/Category.ts
@@ -1,7 +1,7 @@
 import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
 import { v4 as uuid} from "uuid"
 
-@Entity("categorires")
+@Entity("categories")
 export class Category{
 
     @PrimaryColumn()
--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,5 +1,5 @@
 import { getRepository } from "typeorm";
-import { Category } from "../entities/Category";
+import { Category } from "./../entities/Category";
 
 type CategoryRequest = {
     name: string;
@@ -7,10 +7,13 @@ type CategoryRequest = {
 }
 
 export class CreateCategoryService {
-    async execute({name, description}:CategoryRequest): Promise<Category>{
+    async execute({name, description}:CategoryRequest): Promise<Category | Error>{
         const repo = getRepository(Category);
 
-        //
+        // SELECT * FROM CATEGORIES WHERE NAME = "NAME" LIMIT 1
+        if (await repo.findOne({ name })){
+            return new Error("Category already exists");
+        }
 
         const category = repo.create({
             name,
